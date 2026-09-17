f4a8d6f5079e35b8f98b1324b4bee9b2abcf3a1d
Dmitry029 <kdp.029029@gmail.com>
2026-09-17 17:12:05 +0300

    исправил локатор

--- src/test/java/tests/local/SearchTests.java
+++ src/test/java/tests/local/SearchTests.java
@@ -29,5 +29,4 @@ public class SearchTests extends TestBase {
         step("Verify content found", () ->
                 $$(id("org.wikipedia:id/page_list_item_title")).shouldHave(sizeGreaterThan(0)));
     }
-
 }
